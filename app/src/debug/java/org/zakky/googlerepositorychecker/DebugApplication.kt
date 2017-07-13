ba4b2add6065d69b0859b49576ff0eff705b6d29
Makoto Yamazaki <makoto1975@gmail.com>
2017-07-14 01:09:36 +0900

    update Toothpick configuration

--- app/src/debug/java/org/zakky/googlerepositorychecker/DebugApplication.kt
+++ app/src/debug/java/org/zakky/googlerepositorychecker/DebugApplication.kt
@@ -1,10 +1,19 @@
 package org.zakky.googlerepositorychecker
 
+import org.zakky.googlerepositorychecker.toothpick.FactoryRegistry
+import org.zakky.googlerepositorychecker.toothpick.MemberInjectorRegistry
 import toothpick.Toothpick
 import toothpick.configuration.Configuration
+import toothpick.registries.FactoryRegistryLocator
+import toothpick.registries.MemberInjectorRegistryLocator
 
 class DebugApplication : MyApplication() {
     override fun setToothpickConfiguration() {
-        Toothpick.setConfiguration(Configuration.forDevelopment())
+        val configForDevelop = Configuration.forDevelopment()
+                .disableReflection()
+                .preventMultipleRootScopes()
+        Toothpick.setConfiguration(configForDevelop)
+        FactoryRegistryLocator.setRootRegistry(FactoryRegistry())
+        MemberInjectorRegistryLocator.setRootRegistry(MemberInjectorRegistry());
     }
 }
--- app/src/main/java/org/zakky/googlerepositorychecker/MyApplication.kt
+++ app/src/main/java/org/zakky/googlerepositorychecker/MyApplication.kt
@@ -4,8 +4,12 @@ import android.annotation.SuppressLint
 import android.app.Application
 import io.realm.Realm
 import org.zakky.googlerepositorychecker.toothpick.ApplicationModule
+import org.zakky.googlerepositorychecker.toothpick.FactoryRegistry
+import org.zakky.googlerepositorychecker.toothpick.MemberInjectorRegistry
 import toothpick.Toothpick
 import toothpick.configuration.Configuration
+import toothpick.registries.FactoryRegistryLocator
+import toothpick.registries.MemberInjectorRegistryLocator
 
 @SuppressLint("Registered")
 open class MyApplication : Application() {
@@ -22,6 +26,12 @@ open class MyApplication : Application() {
         setupToothpick()
     }
 
+    override fun onTerminate() {
+        super.onTerminate()
+
+        Toothpick.closeScope(APP_SCOPE_NAME)
+    }
+
     private fun setupToothpick() {
         setToothpickConfiguration()
 
@@ -30,6 +40,11 @@ open class MyApplication : Application() {
     }
 
     open protected fun setToothpickConfiguration() {
-        Toothpick.setConfiguration(Configuration.forProduction().disableReflection())
+        val config = Configuration.forProduction()
+                .disableReflection()
+
+        Toothpick.setConfiguration(config)
+        FactoryRegistryLocator.setRootRegistry(FactoryRegistry())
+        MemberInjectorRegistryLocator.setRootRegistry(MemberInjectorRegistry());
     }
 }
