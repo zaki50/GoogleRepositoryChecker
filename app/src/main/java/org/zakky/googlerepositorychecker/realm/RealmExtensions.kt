bdf3d4d2ffa1a7a085d1fa17887d0d189f107c25
Makoto Yamazaki <makoto1975@gmail.com>
2017-07-20 15:01:19 +0900

    remove dependency to org.jetbrains.kotlin:kotlin-reflect

--- app/src/main/java/org/zakky/googlerepositorychecker/realm/RealmExtensions.kt
+++ app/src/main/java/org/zakky/googlerepositorychecker/realm/RealmExtensions.kt
@@ -5,7 +5,6 @@ package org.zakky.googlerepositorychecker.realm
 import io.realm.*
 import java.util.*
 import java.util.concurrent.atomic.AtomicReference
-import kotlin.reflect.KClass
 import kotlin.reflect.KMutableProperty1
 
 fun RealmModel.deleteFromRealm() {
