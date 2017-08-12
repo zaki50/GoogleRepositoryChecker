aee707fc0cea1149d6df9af154bedd89f52d9d64
Makoto Yamazaki <makoto1975@gmail.com>
2017-08-12 18:37:26 +0900

    add more extension functions for model classes

--- app/src/main/kotlin/org/zakky/googlerepositorychecker/realm/extensions/RealmModelExtensions.kt
+++ app/src/main/kotlin/org/zakky/googlerepositorychecker/realm/extensions/RealmModelExtensions.kt
@@ -11,3 +11,40 @@ import kotlin.reflect.KMutableProperty1
 fun RealmModel.deleteFromRealm() {
     RealmObject.deleteFromRealm(this)
 }
+
+fun RealmModel.isValid(): Boolean {
+    return RealmObject.isValid(this)
+}
+
+fun RealmModel.isManaged(): Boolean {
+    return RealmObject.isManaged(this)
+}
+
+fun RealmModel.isLoaded(): Boolean {
+    return RealmObject.isLoaded(this)
+}
+
+fun RealmModel.load(): Boolean {
+    return RealmObject.load(this)
+}
+
+fun <E : RealmModel> E.addChangeListener(listener: RealmChangeListener<E>) {
+    RealmObject.addChangeListener(this, listener)
+}
+
+fun <E : RealmModel> E.addChangeListener(listener: RealmObjectChangeListener<E>) {
+    RealmObject.addChangeListener(this, listener)
+}
+
+fun  <E : RealmModel> E.removeChangeListener(listener: RealmChangeListener<E>) {
+    RealmObject.removeChangeListener(this, listener)
+}
+
+fun  <E : RealmModel> E.removeChangeListener(listener: RealmObjectChangeListener<E>) {
+    RealmObject.removeChangeListener(this, listener)
+}
+
+fun RealmModel.removeAllChangeListeners() {
+    return RealmObject.removeAllChangeListeners(this)
+}
+
